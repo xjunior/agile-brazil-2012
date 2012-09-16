8325bef29a909c48f1225bb52ace8fe68a289415
Daniel Medeiros <medeiros@lavabit.com>
2012-09-16 13:11:33 -0300

    organizacao do mapa mental da lightning talk #2

--- freemind/2012-09-06-#04-lightning-talks-2.mm
+++ freemind/2012-09-06-#04-lightning-talks-2.mm
@@ -1,27 +1,35 @@
 <map version="0.9.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1346931069711" ID="ID_1541151578" MODIFIED="1346931104561" TEXT="Lightning talks #5">
-<node CREATED="1346946576704" ID="ID_1920115024" MODIFIED="1346946578251" POSITION="left" TEXT="primeira">
-<node CREATED="1346946496811" ID="ID_872352106" MODIFIED="1346946500586" TEXT="o que &#xe9; medido &#xe9; entregue">
-<node CREATED="1346946507378" ID="ID_1520082006" MODIFIED="1346946513277" TEXT="ex da fabrica de prego sovietica"/>
+<node CREATED="1346931069711" ID="ID_1541151578" MODIFIED="1347811742302" TEXT="Lightning talks #2">
+<node CREATED="1346946576704" FOLDED="true" ID="ID_1920115024" MODIFIED="1347811867573" POSITION="left" TEXT="primeira">
+<node CREATED="1347811509385" ID="ID_1855165142" MODIFIED="1347811529204" TEXT="sobre m&#xe9;tricas e medi&#xe7;&#xf5;es"/>
+<node CREATED="1346946496811" FOLDED="true" ID="ID_872352106" MODIFIED="1347811675905" TEXT="o que &#xe9; medido &#xe9; entregue">
+<node CREATED="1346946507378" ID="ID_1520082006" MODIFIED="1347811612030" TEXT="cita anedota da fabrica de prego sovi&#xe9;tica">
+<node CREATED="1347811627351" ID="ID_1531540824" MODIFIED="1347811640379" TEXT="a efici&#xea;ncia era pelo peso total de produ&#xe7;&#xe3;o mensal"/>
+<node CREATED="1347811641021" ID="ID_687547021" MODIFIED="1347811660416" TEXT="a f&#xe1;brica passou a gerar pregos imensos que n&#xe3;o serviam para nada">
+<node CREATED="1347811664161" ID="ID_1341617528" MODIFIED="1347811671200" TEXT="mas atendia &#xe0; m&#xe9;trica de efici&#xea;ncia"/>
 </node>
-<node CREATED="1346946571455" ID="ID_1130809817" MODIFIED="1346946575419" TEXT="eh muito rapido para se anotar"/>
-<node CREATED="1346947002681" ID="ID_1606351232" MODIFIED="1346947016848" TEXT="questao da medicao e do valor entregue"/>
+</node>
+</node>
 <node CREATED="1346946982102" ID="ID_1997489517" MODIFIED="1346947026535" TEXT="mais refer&#xea;ncia em http://amzn.to/manageperf"/>
+<node CREATED="1347811718259" ID="ID_598198369" LINK="http://amzn.to/manageperf" MODIFIED="1347811737349" TEXT="citou o livro: &quot;Measure and Managing Performance in Organizations&quot;"/>
 </node>
-<node CREATED="1346947027914" ID="ID_397367148" MODIFIED="1346947029102" POSITION="right" TEXT="segunda">
-<node CREATED="1346947124938" ID="ID_265816167" MODIFIED="1346947131434" TEXT="visao de que metricas sao inuteis">
-<node CREATED="1346947135379" ID="ID_123289317" MODIFIED="1346947144726" TEXT="em equipe agil via desta forma"/>
+<node CREATED="1346947027914" FOLDED="true" ID="ID_397367148" MODIFIED="1347811866853" POSITION="right" TEXT="segunda">
+<node CREATED="1347811745937" ID="ID_1457948979" MODIFIED="1347811756358" TEXT="tamb&#xe9;m sobre m&#xe9;tricas e medi&#xe7;&#xf5;es">
+<node CREATED="1347811817854" ID="ID_494321984" MODIFIED="1347811835214" TEXT="sobre um desenvolvedor de equipe &#xe1;gil que pessoa a ver valor em m&#xe9;tricas"/>
+</node>
+<node CREATED="1346947124938" ID="ID_265816167" MODIFIED="1347811841353" TEXT="vis&#xe3;o inicial: m&#xe9;tricas sao in&#xfa;teis">
+<node CREATED="1347811849988" ID="ID_1265655358" MODIFIED="1347811863780" TEXT="opini&#xe3;o mudou depois, num projeto pessoal"/>
 </node>
 <node CREATED="1346947170331" ID="ID_1633209719" MODIFIED="1346947176640" TEXT="surgiu projeto pessoal">
 <node CREATED="1346947177317" ID="ID_355648207" MODIFIED="1346947179003" TEXT="perder peso"/>
-<node CREATED="1346947184142" ID="ID_1168198968" MODIFIED="1346947191382" TEXT="nutricionista comecou a cobrar metricas"/>
+<node CREATED="1346947184142" ID="ID_1168198968" MODIFIED="1347811796544" TEXT="nutricionista comecou a cobrar m&#xe9;tricas"/>
 <node CREATED="1346947196263" ID="ID_598831459" MODIFIED="1346947197762" TEXT="nao minta">
 <node CREATED="1346947208088" ID="ID_210404234" MODIFIED="1346947213074" TEXT="se houver desvios, aceite"/>
 <node CREATED="1346947198439" ID="ID_1572196268" MODIFIED="1346947207068" TEXT="se houver motivo, anote"/>
 </node>
 </node>
-<node CREATED="1346947231722" ID="ID_542936907" MODIFIED="1346947232925" TEXT="grafico">
+<node CREATED="1346947231722" ID="ID_542936907" MODIFIED="1347811801761" TEXT="gr&#xe1;fico">
 <node CREATED="1346947233430" ID="ID_425818461" MODIFIED="1346947239282" TEXT="ajuda a ver tend&#xea;ncias, n&#xe3;o certezas"/>
 </node>
 <node CREATED="1346947252290" ID="ID_1086905164" MODIFIED="1346947254336" TEXT="mudan&#xe7;as">
@@ -30,7 +38,7 @@
 <node CREATED="1346947299851" ID="ID_1332307526" MODIFIED="1346947301367" TEXT="objetivo">
 <node CREATED="1346947301918" ID="ID_301265599" MODIFIED="1346947304619" TEXT="nunca o perca de vista">
 <node CREATED="1346947305655" ID="ID_1911697457" MODIFIED="1346947312474" TEXT="n&#xe3;o era ter um gr&#xe1;fico bom"/>
-<node CREATED="1346947313431" ID="ID_1211242213" MODIFIED="1346947320726" TEXT="era conseguir perder o peso"/>
+<node CREATED="1346947313431" ID="ID_1211242213" MODIFIED="1347811812611" TEXT="era conseguir perder peso"/>
 </node>
 </node>
 <node CREATED="1346947077803" ID="ID_1737534412" MODIFIED="1346947326272" TEXT="sobre m&#xe9;tricas">
