3cb8725b661d14d37cf4bd13bfe3f43a40d5e788
Daniel Medeiros <medeiros@lavabit.com>
2012-09-16 11:37:32 -0300

    organizacao do mapa mental da apresentacao de qualidade: pensando fora da caixa

--- freemind/2012-09-05-#04-qualidade-fora-da-caixa.mm
+++ freemind/2012-09-05-#04-qualidade-fora-da-caixa.mm
@@ -1,102 +1,118 @@
 <map version="0.9.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1347703299845" ID="ID_892107958" MODIFIED="1347703384492" TEXT="Qualidade: pensando fora da caixa">
-<node CREATED="1347703384934" ID="ID_943085156" MODIFIED="1347703397098" POSITION="left" TEXT="palestrante: Jorge Alberto Diz">
-<node CREATED="1347703404451" ID="ID_1483266794" MODIFIED="1347703406176" TEXT="maps.com.br"/>
+<node CREATED="1347703384934" FOLDED="true" ID="ID_943085156" MODIFIED="1347806216222" POSITION="left" TEXT="palestrante: Jorge Alberto Diz">
+<node CREATED="1347703404451" ID="ID_1483266794" LINK="http://www.maps.com.br" MODIFIED="1347805149911" TEXT="Da empresa Maps"/>
 </node>
-<node CREATED="1347703402260" ID="ID_379079766" MODIFIED="1347703409952" POSITION="left" TEXT="O que anda acontecendo?">
-<node CREATED="1347703415322" ID="ID_601773375" MODIFIED="1347703420703" TEXT="software como servi&#xe7;o">
-<node CREATED="1347703423801" ID="ID_1921232049" MODIFIED="1347703425967" TEXT="cada vez mais"/>
-<node CREATED="1347703437096" ID="ID_439552471" MODIFIED="1347703453284" TEXT="mecanismos de atualiza&#xe7;&#xe3;o em c&#xf3;digo local, de forma autom&#xe1;tica">
+<node CREATED="1347703402260" FOLDED="true" ID="ID_379079766" MODIFIED="1347805263679" POSITION="left" TEXT="O que anda acontecendo?">
+<node CREATED="1347703415322" FOLDED="true" ID="ID_601773375" MODIFIED="1347805194066" TEXT="software como servi&#xe7;o">
+<node CREATED="1347703423801" ID="ID_1921232049" MODIFIED="1347805186376" TEXT="cada vez mais software &#xe9; visto como um servi&#xe7;o"/>
+<node CREATED="1347703437096" ID="ID_439552471" MODIFIED="1347805192240" TEXT="cita mecanismos de atualiza&#xe7;&#xe3;o em c&#xf3;digo local, de forma autom&#xe1;tica">
 <node CREATED="1347703453710" ID="ID_1994269254" MODIFIED="1347703456908" TEXT="ex: google chrome"/>
 </node>
 </node>
-<node CREATED="1347703465853" ID="ID_1943128766" MODIFIED="1347703470322" TEXT="seguran&#xe7;a f&#xed;sica">
+<node CREATED="1347703465853" FOLDED="true" ID="ID_1943128766" MODIFIED="1347805200251" TEXT="seguran&#xe7;a f&#xed;sica">
+<node CREATED="1347703483211" ID="ID_1262941408" MODIFIED="1347805198808" TEXT="confia-se so software em situa&#xe7;&#xf5;es de seguran&#xe7;a f&#xed;sica"/>
 <node CREATED="1347703470820" ID="ID_560205428" MODIFIED="1347703480593" TEXT="ex: linha amarela; sem maquinista"/>
-<node CREATED="1347703483211" ID="ID_1262941408" MODIFIED="1347703492087" TEXT="confia-se so software em situa&#xe7;&#xf5;es de seguran&#xe7;a f&#xed;sica"/>
 </node>
-<node CREATED="1347703494778" ID="ID_477658564" MODIFIED="1347703498175" TEXT="privacidade">
+<node CREATED="1347703494778" FOLDED="true" ID="ID_477658564" MODIFIED="1347805207925" TEXT="privacidade">
 <node CREATED="1347703498673" ID="ID_639548576" MODIFIED="1347703501583" TEXT="exposi&#xe7;&#xe3;o cada vez maior"/>
+<node CREATED="1347805203605" ID="ID_1217026064" MODIFIED="1347805207355" TEXT="exemplo: redes sociais"/>
 </node>
-<node CREATED="1347703504513" ID="ID_651292845" MODIFIED="1347703505662" TEXT="tempo">
+<node CREATED="1347703504513" FOLDED="true" ID="ID_651292845" MODIFIED="1347805210902" TEXT="tempo">
 <node CREATED="1347703506152" ID="ID_1255845600" MODIFIED="1347703511845" TEXT="n&#xe3;o se pode demorar para criar software"/>
 </node>
-<node CREATED="1347703515968" ID="ID_765170055" MODIFIED="1347703517997" TEXT="ubiquidade">
+<node CREATED="1347703515968" FOLDED="true" ID="ID_765170055" MODIFIED="1347805212990" TEXT="ubiquidade">
 <node CREATED="1347703518431" ID="ID_796483054" MODIFIED="1347703524750" TEXT="portabilidade em muitos dispositivos"/>
 </node>
-<node CREATED="1347703527198" ID="ID_129813368" MODIFIED="1347703531260" TEXT="complexidade">
-<node CREATED="1347703532046" ID="ID_1360761850" MODIFIED="1347703534518" TEXT="cada vez mais"/>
+<node CREATED="1347703527198" FOLDED="true" ID="ID_129813368" MODIFIED="1347805220183" TEXT="complexidade">
+<node CREATED="1347703532046" ID="ID_1360761850" MODIFIED="1347805219357" TEXT="cada vez mais sistemas s&#xe3;o complexos"/>
 </node>
-<node CREATED="1347703536637" ID="ID_1904303995" MODIFIED="1347703539699" TEXT="procurar defeitos">
-<node CREATED="1347703540173" ID="ID_1563205515" MODIFIED="1347703543234" TEXT="ideia de controle de qualidade">
+<node CREATED="1347703536637" FOLDED="true" ID="ID_1904303995" MODIFIED="1347805232047" TEXT="procurar defeitos">
+<node CREATED="1347703540173" ID="ID_1563205515" MODIFIED="1347703543234" TEXT="ideia de controle de qualidade"/>
 <node CREATED="1347703547252" ID="ID_1915905262" MODIFIED="1347703562096" TEXT="problema: nem todo bug tem a mesma import&#xe2;ncia e cada vez mais surgem bugs"/>
 </node>
+<node CREATED="1347703569530" FOLDED="true" ID="ID_822355325" MODIFIED="1347805247476" TEXT="sexagem de c&#xf3;digo">
+<node CREATED="1347703573129" ID="ID_832764839" MODIFIED="1347805246425" TEXT="definir se um dado c&#xf3;digo &#xe9; bug ou feature?"/>
 </node>
-<node CREATED="1347703569530" ID="ID_822355325" MODIFIED="1347703572714" TEXT="sexagem de c&#xf3;digo">
-<node CREATED="1347703573129" ID="ID_832764839" MODIFIED="1347703576839" TEXT="bug ou feature?"/>
 </node>
-<node CREATED="1347703579417" ID="ID_1086808141" MODIFIED="1347703581502" TEXT="valores &#xe1;geis">
+<node CREATED="1347703579417" FOLDED="true" ID="ID_1086808141" MODIFIED="1347805262135" POSITION="left" TEXT="valores &#xe1;geis">
 <node CREATED="1347703584424" ID="ID_1645219627" MODIFIED="1347703588678" TEXT="fluidez de comunica&#xe7;&#xe3;o"/>
 <node CREATED="1347703589871" ID="ID_1967316341" MODIFIED="1347703592053" TEXT="transpar&#xea;ncia"/>
 <node CREATED="1347703593063" ID="ID_279825757" MODIFIED="1347703596101" TEXT="foco em entrega de valor"/>
 <node CREATED="1347703597151" ID="ID_231532508" MODIFIED="1347703623314" TEXT="sustentabilidade do esfor&#xe7;o">
-<node CREATED="1347703619124" ID="ID_1474692734" MODIFIED="1347703620851" TEXT="40 horas"/>
+<node CREATED="1347703619124" ID="ID_1474692734" MODIFIED="1347805258148" TEXT="trabalho de 40 horas"/>
 </node>
 <node CREATED="1347703628203" ID="ID_791243102" MODIFIED="1347703635209" TEXT="compreens&#xe3;o dos fatores humanos"/>
 <node CREATED="1347703637258" ID="ID_221477117" MODIFIED="1347703641202" TEXT="reavalia&#xe7;&#xe3;o constante"/>
 </node>
-<node CREATED="1347703652881" ID="ID_100677962" MODIFIED="1347703663006" TEXT="Gest&#xe3;o de projeto de software">
+<node CREATED="1347805287051" FOLDED="true" ID="ID_1309532801" MODIFIED="1347805786542" POSITION="left" TEXT="sobre modelo de f&#xe1;brica">
 <node CREATED="1347703665471" ID="ID_1341103604" MODIFIED="1347703676548" TEXT="muitas vezes se usa o modelo de f&#xe1;brica do s&#xe9;culo XIX"/>
 <node CREATED="1347703678766" ID="ID_198335585" MODIFIED="1347703684724" TEXT="pessoas s&#xe3;o chamadas de &quot;recursos&quot;"/>
 <node CREATED="1347703688461" ID="ID_477744708" MODIFIED="1347703695596" TEXT="usa-se ainda a ideia de linha de montagem">
 <node CREATED="1347703697388" ID="ID_1067283324" MODIFIED="1347703702043" TEXT="entra requisito e sai c&#xf3;digo">
 <node CREATED="1347703702515" ID="ID_1958254118" MODIFIED="1347703704762" TEXT="nem produto sai"/>
 </node>
-<node CREATED="1347703709834" ID="ID_843655144" MODIFIED="1347703711939" TEXT="modelo cascata"/>
+<node CREATED="1347703709834" ID="ID_843655144" MODIFIED="1347805312863" TEXT="modelo cascata segue esta linha"/>
 </node>
+</node>
+<node CREATED="1347805341982" FOLDED="true" ID="ID_677653574" MODIFIED="1347805787214" POSITION="right" TEXT="sobre modelos de software">
 <node CREATED="1347703717706" ID="ID_1963175992" MODIFIED="1347703723528" TEXT="Regra de 10x de Myers">
 <node CREATED="1347703726121" ID="ID_62097320" MODIFIED="1347703747157" TEXT="considerando uma cascata, a cada est&#xe1;gio multiplica-se por 10 o custo de resolu&#xe7;&#xe3;o do problema"/>
 </node>
-<node CREATED="1347703750198" ID="ID_687705896" MODIFIED="1347703767411" TEXT="Foi criado o modelo V, a partir do modelo em cascata (depende dele)">
+<node CREATED="1347703750198" ID="ID_687705896" LINK="http://en.wikipedia.org/wiki/V-Model_%28software_development%29" MODIFIED="1347805432335" TEXT="Foi criado o modelo V">
+<node CREATED="1347805421492" ID="ID_935890940" MODIFIED="1347805427558" TEXT="a partir do modelo em cascata"/>
+<node CREATED="1347805408626" ID="ID_1686532328" MODIFIED="1347805413808" TEXT="depende do modelo em cascata"/>
 <node CREATED="1347703770118" ID="ID_759610914" MODIFIED="1347703778170" TEXT="teste acontece de acordo com o nivel"/>
-<node CREATED="1347703779835" ID="ID_569998292" MODIFIED="1347703781785" TEXT="pesquisar modelo"/>
 </node>
-<node CREATED="1347703788746" ID="ID_632205251" MODIFIED="1347703797952" TEXT="Pareto">
-<node CREATED="1347703798305" ID="ID_677983144" MODIFIED="1347703804799" TEXT="regra 20/80">
-<node CREATED="1347703805272" ID="ID_1852843029" MODIFIED="1347703814582" TEXT="20% de algo tem 80% de import&#xe2;ncia">
-<node CREATED="1347703817863" ID="ID_1083221623" MODIFIED="1347703819990" TEXT="pesquisar"/>
-</node>
-</node>
-</node>
-<node CREATED="1347703826054" ID="ID_486801271" MODIFIED="1347703832119" TEXT="recursos escassos">
-<node CREATED="1347703847412" ID="ID_833154090" MODIFIED="1347703850667" TEXT="em import&#xe2;ncia:">
-<node CREATED="1347703832525" ID="ID_1239885848" MODIFIED="1347703872032" TEXT="tempo, aten&#xe7;&#xe3;o, capacidade de aprendizagem, remunera&#xe7;&#xe3;o das pessoas, licen&#xe7;as de software, hardware"/>
+<node CREATED="1347703788746" FOLDED="true" ID="ID_632205251" LINK="http://en.wikipedia.org/wiki/Pareto_principle" MODIFIED="1347805695837" TEXT="Princ&#xed;pio de Pareto">
+<node CREATED="1347703798305" ID="ID_677983144" MODIFIED="1347805537858" TEXT="ou &quot;regra 80/20&quot;"/>
+<node CREATED="1347805534538" ID="ID_1021347232" MODIFIED="1347805559439" TEXT="80% dos efeitos vem de 20% das causas">
+<node CREATED="1347805635500" ID="ID_1162684291" MODIFIED="1347805693168" TEXT="ou seja, 80% do trabalho do cliente acontece em 20% das funcionalidades"/>
+<node CREATED="1347805667122" ID="ID_641312967" MODIFIED="1347805672123" TEXT="ou algo nesta linha"/>
 </node>
 </node>
-<node CREATED="1347703877152" ID="ID_995292224" MODIFIED="1347703881911" TEXT="melhoria dos recursos">
+</node>
+<node CREATED="1347805705529" FOLDED="true" ID="ID_73740675" MODIFIED="1347805789104" POSITION="right" TEXT="sobre recursos">
+<node CREATED="1347703877152" FOLDED="true" ID="ID_995292224" MODIFIED="1347805765306" TEXT="melhoria dos recursos">
 <node CREATED="1347703882432" ID="ID_600990247" MODIFIED="1347703887015" TEXT="atrav&#xe9;s de itera&#xe7;&#xf5;es mais curtas">
 <node CREATED="1347703889799" ID="ID_483838506" MODIFIED="1347703900264" TEXT="problema nao est&#xe1; na qtde, e sim na prioriza&#xe7;&#xe3;o"/>
 <node CREATED="1347703903670" ID="ID_1556555191" MODIFIED="1347703938715" TEXT="retorno sobre o investimento antes"/>
 <node CREATED="1347703913453" ID="ID_841576908" MODIFIED="1347703918534" TEXT="o que n&#xe3;o serve fica para tr&#xe1;s"/>
 </node>
 </node>
+<node CREATED="1347703826054" FOLDED="true" ID="ID_486801271" MODIFIED="1347805764554" TEXT="recursos escassos">
+<node CREATED="1347703847412" ID="ID_833154090" MODIFIED="1347703850667" TEXT="em import&#xe2;ncia:">
+<node CREATED="1347805735167" ID="ID_1384448232" MODIFIED="1347805736191" TEXT="tempo"/>
+<node CREATED="1347805737055" ID="ID_1674626074" MODIFIED="1347805738671" TEXT="aten&#xe7;&#xe3;o"/>
+<node CREATED="1347805739400" ID="ID_399689424" MODIFIED="1347805744274" TEXT="capacidade de aprendizagem"/>
+<node CREATED="1347805745457" ID="ID_558282677" MODIFIED="1347805751156" TEXT="remunera&#xe7;&#xe3;o de pessoas"/>
+<node CREATED="1347805752058" ID="ID_77793371" MODIFIED="1347805754996" TEXT="licen&#xe7;a de software"/>
+<node CREATED="1347805755571" ID="ID_771257531" MODIFIED="1347805756758" TEXT="hardware"/>
+</node>
+</node>
+</node>
+<node CREATED="1347805805276" FOLDED="true" ID="ID_1277264940" MODIFIED="1347806177887" POSITION="right" TEXT="sobre qualidade e estimativas">
 <node CREATED="1347703944265" ID="ID_1523269247" MODIFIED="1347703950808" TEXT="minha qualidade n&#xe3;o &#xe9; a sua"/>
 <node CREATED="1347703951800" ID="ID_336409946" MODIFIED="1347703953624" TEXT="estimativa">
+<node CREATED="1347703960911" ID="ID_287180828" MODIFIED="1347805823419" TEXT="&#xe9; um problema"/>
 <node CREATED="1347703954088" ID="ID_1914862378" MODIFIED="1347703960207" TEXT="porque o modelo &#xe9; or&#xe7;ar e ent&#xe3;o entregar"/>
-<node CREATED="1347703960911" ID="ID_287180828" MODIFIED="1347703965352" TEXT="&#xe9; um problema"/>
+<node CREATED="1347704258024" ID="ID_1899239841" MODIFIED="1347704260444" TEXT="estimativas falham"/>
 </node>
-<node CREATED="1347703973262" ID="ID_886274486" MODIFIED="1347703975670" TEXT="expectativas:"/>
-<node CREATED="1347703982901" ID="ID_1203879589" MODIFIED="1347703987271" TEXT="din&#xe2;mica de sistemas">
-<node CREATED="1347703987797" ID="ID_1643064470" MODIFIED="1347703993484" TEXT="como no chuveiro: feedback demora"/>
-</node>
-<node CREATED="1347703996950" ID="ID_1625240675" MODIFIED="1347704004636" TEXT="ciclo PDSA para melhoria de qualidade">
-<node CREATED="1347704007938" ID="ID_415316311" MODIFIED="1347704019873" TEXT="Plan, Do, Act">
-<node CREATED="1347704020225" ID="ID_776803901" MODIFIED="1347704021355" TEXT="pesquisar"/>
-</node>
+<node CREATED="1347703996950" ID="ID_1625240675" LINK="http://en.wikipedia.org/wiki/PDCA" MODIFIED="1347805904695" TEXT="ciclo PDSA para melhoria de qualidade">
+<node CREATED="1347704007938" ID="ID_415316311" MODIFIED="1347805883593" TEXT="Plan, Do, Study, Act"/>
+<node CREATED="1347805914713" ID="ID_250465727" MODIFIED="1347805921237" TEXT="para melhoria cont&#xed;nua"/>
 </node>
 <node CREATED="1347704027009" ID="ID_864505634" MODIFIED="1347704030082" TEXT="qualidade x produtividade">
 <node CREATED="1347704032056" ID="ID_672545529" MODIFIED="1347704037999" TEXT="fal&#xe1;cia de que com qualidade aumenta o custo"/>
 </node>
+<node CREATED="1347704148707" ID="ID_775769104" MODIFIED="1347806091760" TEXT="Controle de qualidade -&gt; Garantia de qualidade -&gt; Promo&#xe7;&#xe3;o de qualidade"/>
+<node CREATED="1347704181800" ID="ID_800771760" MODIFIED="1347704187424" TEXT="Caixa preta x Caixa branca">
+<node CREATED="1347704191271" ID="ID_767991710" MODIFIED="1347704198192" TEXT="no fundo, n&#xe3;o tem tanta import&#xe2;ncia"/>
+<node CREATED="1347704202414" ID="ID_350593350" MODIFIED="1347806104274" TEXT="o importante &#xe9; maior preocupa&#xe7;&#xe3;o sobre como funciona por dentro"/>
+</node>
+</node>
+<node CREATED="1347806021164" FOLDED="true" ID="ID_1613520937" MODIFIED="1347806182448" POSITION="right" TEXT="sobre erros">
 <node CREATED="1347704040319" ID="ID_1455586403" MODIFIED="1347704041911" TEXT="erro:">
 <node CREATED="1347704042391" ID="ID_111265909" MODIFIED="1347704050494" TEXT="vacilo que algu&#xe9;m comete"/>
 </node>
@@ -106,22 +122,17 @@
 <node CREATED="1347704066380" ID="ID_192094762" MODIFIED="1347704067407" TEXT="falha">
 <node CREATED="1347704068052" ID="ID_1518414442" MODIFIED="1347704076181" TEXT="&#xe9; um desastre que ocorre"/>
 </node>
-<node CREATED="1347704085218" ID="ID_293319986" MODIFIED="1347704109929" TEXT="uma vez que ocorre o erro, queremos que a falha se apresente da frma mais r&#xe1;pida poss&#xed;vel (com testes)">
+<node CREATED="1347704085218" ID="ID_293319986" MODIFIED="1347806060287" TEXT="uma vez que ocorre o erro">
+<node CREATED="1347806061556" ID="ID_1020637973" MODIFIED="1347806062946" TEXT="queremos que a falha se apresente da forma mais r&#xe1;pida poss&#xed;vel (com testes)"/>
 <node CREATED="1347704110839" ID="ID_285394526" MODIFIED="1347704119511" TEXT="para que n&#xe3;o aconte&#xe7;a em produ&#xe7;&#xe3;o"/>
 <node CREATED="1347704120286" ID="ID_865606087" MODIFIED="1347704126617" TEXT="precisa, dado uma falha, ter um diagn&#xf3;stico"/>
 </node>
 <node CREATED="1347704130045" ID="ID_196539532" MODIFIED="1347704146478" TEXT="preciso ter gest&#xe3;o de ocorr&#xea;ncias, mas n&#xe3;o necessariamente preciso corrigir tudo"/>
-<node CREATED="1347704148707" ID="ID_775769104" MODIFIED="1347704174557" TEXT="Controle de qualidade -&gt; garantia de qualidade -&gt; Promo&#xe7;&#xe3;o de qualidade"/>
-<node CREATED="1347704181800" ID="ID_800771760" MODIFIED="1347704187424" TEXT="Caixa preta x Caixa branca">
-<node CREATED="1347704191271" ID="ID_767991710" MODIFIED="1347704198192" TEXT="no fundo, n&#xe3;o tem tanta import&#xe2;ncia"/>
-<node CREATED="1347704202414" ID="ID_350593350" MODIFIED="1347704222748" TEXT="o importante &#xe9; maior preocupa&#xe7;&#xe3;o como funciona por dentro"/>
-</node>
 <node CREATED="1347704231282" ID="ID_971187161" MODIFIED="1347704247225" TEXT="Dist&#xe2;ncia entre erro e corre&#xe7;&#xe3;o geram maior custo de corrigir e risco de n&#xe3;o corrigir"/>
-<node CREATED="1347704250968" ID="ID_714050495" MODIFIED="1347704255384" TEXT="registro n&#xe3;o garante comunica&#xe7;&#xe3;o"/>
-<node CREATED="1347704258024" ID="ID_1899239841" MODIFIED="1347704260444" TEXT="estimativas falham"/>
-<node CREATED="1347704263335" ID="ID_1462145626" MODIFIED="1347704272703" TEXT="porque n&#xe3;o desapegamos do modelo em cascata?"/>
-<node CREATED="1347704277550" ID="ID_35018386" MODIFIED="1347704288101" TEXT="Porque tanto esfor&#xe7;o em teste automatizado de tela?"/>
 </node>
+<node CREATED="1347806145757" FOLDED="true" ID="ID_218509546" MODIFIED="1347806160780" POSITION="right" TEXT="ret&#xf3;rica">
+<node CREATED="1347704263335" ID="ID_1462145626" MODIFIED="1347806156940" TEXT="por que n&#xe3;o desapegamos do modelo em cascata?"/>
+<node CREATED="1347704277550" ID="ID_35018386" MODIFIED="1347806155420" TEXT="por que tanto esfor&#xe7;o em teste automatizado de tela?"/>
 </node>
 </node>
 </map>
