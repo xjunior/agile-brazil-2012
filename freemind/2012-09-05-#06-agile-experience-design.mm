82d84e7ba95bb5cb305e2d11ff173ba618123bc6
Daniel Medeiros <medeiros@lavabit.com>
2012-09-16 11:52:29 -0300

    organizacao do mapa mental da apresentacao de agile entre developers e designers

--- freemind/2012-09-05-#06-agile-experience-design.mm
+++ freemind/2012-09-05-#06-agile-experience-design.mm
@@ -1,16 +1,18 @@
 <map version="0.9.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1347704991507" ID="ID_1623308964" MODIFIED="1347705040232" TEXT="Agile experience design: when developers and designers become a match made in heaven">
-<node CREATED="1347705044275" ID="ID_1817630314" MODIFIED="1347705047328" POSITION="left" TEXT="Palestrantes">
+<node CREATED="1347705044275" FOLDED="true" ID="ID_1817630314" MODIFIED="1347806885947" POSITION="left" TEXT="Palestrantes">
 <node CREATED="1347705047803" ID="ID_1902179560" MODIFIED="1347705056075" TEXT="Hugo Corbucci"/>
 <node CREATED="1347705056570" ID="ID_971842395" MODIFIED="1347705064382" TEXT="Juliana Dorneles"/>
+<node CREATED="1347806722447" ID="ID_1973415451" LINK="http://www.thoughtworks.com.br" MODIFIED="1347806738203" TEXT="da Thoughtworks"/>
 </node>
-<node CREATED="1347705081591" ID="ID_1347822532" MODIFIED="1347705086587" POSITION="left" TEXT="O que &#xe9; design &#xe1;gil?"/>
-<node CREATED="1347705091950" ID="ID_725304756" MODIFIED="1347705108177" POSITION="left" TEXT="Prototipar com usu&#xe1;rios e stackholders par obter feedback r&#xe1;pido"/>
-<node CREATED="1347705109996" ID="ID_1262948694" MODIFIED="1347705118680" POSITION="left" TEXT="The agile design toolkit">
+<node CREATED="1347705081591" FOLDED="true" ID="ID_1347822532" MODIFIED="1347806882939" POSITION="left" TEXT="O que &#xe9; design &#xe1;gil?">
+<node CREATED="1347705091950" ID="ID_725304756" MODIFIED="1347806748834" TEXT="Prototipar com usu&#xe1;rios e stackholders para obter feedback r&#xe1;pido"/>
+</node>
+<node CREATED="1347705109996" FOLDED="true" ID="ID_1262948694" MODIFIED="1347806883595" POSITION="left" TEXT="The agile design toolkit">
 <node CREATED="1347705121635" ID="ID_1011621340" MODIFIED="1347705124871" TEXT="pareamento">
 <node CREATED="1347705125362" ID="ID_1062628898" MODIFIED="1347705132209" TEXT="trabalhar em pares, aprendendo e ensinando">
-<node CREATED="1347705132753" ID="ID_178389406" MODIFIED="1347705143206" TEXT="entre deus"/>
+<node CREATED="1347705132753" ID="ID_178389406" MODIFIED="1347806770840" TEXT="entre devs"/>
 <node CREATED="1347705145248" ID="ID_1935716984" MODIFIED="1347705147301" TEXT="e entre &#xe1;reas"/>
 </node>
 </node>
@@ -21,21 +23,22 @@
 </node>
 </node>
 </node>
-<node CREATED="1347705245357" ID="ID_450989034" MODIFIED="1347705249452" POSITION="left" TEXT="do design ao c&#xf3;digo">
+<node CREATED="1347705245357" FOLDED="true" ID="ID_450989034" MODIFIED="1347806884115" POSITION="left" TEXT="do design ao c&#xf3;digo">
 <node CREATED="1347705250229" ID="ID_1261199138" MODIFIED="1347705262746" TEXT="tentar algumas coisas em c&#xf3;digo, e descartar se n&#xe3;o der certo"/>
+<node CREATED="1347806835428" ID="ID_99373835" MODIFIED="1347806852117" TEXT="a ideia de experimenta&#xe7;&#xe3;o e valida&#xe7;&#xe3;o do cliente, com op&#xe7;&#xe3;o de descartar"/>
 </node>
-<node CREATED="1347705266051" ID="ID_294455353" MODIFIED="1347705267953" POSITION="left" TEXT="retrospectiva">
-<node CREATED="1347705269595" ID="ID_1235353997" MODIFIED="1347705281337" TEXT="todos fizeram o melhor trabalho que podiam fazer"/>
+<node CREATED="1347705266051" FOLDED="true" ID="ID_294455353" MODIFIED="1347806884651" POSITION="left" TEXT="na retrospectiva">
+<node CREATED="1347705269595" ID="ID_1235353997" MODIFIED="1347806875178" TEXT="asuumir que todos fizeram o melhor trabalho que podiam fazer"/>
 </node>
-<node CREATED="1347705285289" ID="ID_1876493928" MODIFIED="1347705357367" POSITION="right" TEXT="10 t&#xe9;cnicas para novos projetos">
+<node CREATED="1347705285289" FOLDED="true" ID="ID_1876493928" MODIFIED="1347807100460" POSITION="right" TEXT="10 t&#xe9;cnicas para novos projetos">
 <node CREATED="1347705357985" ID="ID_1572987554" MODIFIED="1347705593367" TEXT="1. time de dev s&#xf3; tiver um prot&#xf3;tipo aprovado e testado"/>
 <node CREATED="1347705406604" ID="ID_673447273" MODIFIED="1347705590942" TEXT="2. crie para todos e voc&#xea; n&#xe3;o est&#xe1; criando para ningu&#xe9;m">
 <node CREATED="1347705538062" ID="ID_46958663" MODIFIED="1347705547325" TEXT="1001 features quando o usu&#xe1;rio s&#xf3; usa uma"/>
+<node CREATED="1347806901592" ID="ID_1119439555" MODIFIED="1347806911505" TEXT="novamente, a id&#xe9;ia de fazer s&#xf3; o que &#xe9; importante no momento"/>
 </node>
 <node CREATED="1347705548421" ID="ID_725785064" MODIFIED="1347705589057" TEXT="3. pessoas criam melhor juntas">
-<node CREATED="1347705562069" ID="ID_1825358568" MODIFIED="1347705573281" TEXT="numa mesa, pessoas escrevendo est&#xf3;rias e pessoas codificando">
+<node CREATED="1347705562069" ID="ID_1825358568" MODIFIED="1347705573281" TEXT="numa mesa, pessoas escrevendo est&#xf3;rias e pessoas codificando"/>
 <node CREATED="1347705573746" ID="ID_1386851261" MODIFIED="1347705577224" TEXT="diferentes pap&#xe9;is"/>
-</node>
 </node>
 <node CREATED="1347705580841" ID="ID_51964483" MODIFIED="1347705605733" TEXT="4. solu&#xe7;&#xf5;es de sucesso na intersec&#xe7;&#xe3;o entre">
 <node CREATED="1347705608310" ID="ID_38589944" MODIFIED="1347705610686" TEXT="&#xe9; poss&#xed;vel"/>
@@ -47,12 +50,13 @@
 <node CREATED="1347705641819" ID="ID_1552268254" MODIFIED="1347705648456" TEXT="quadros, papel e caneta, prot&#xf3;tipos"/>
 </node>
 <node CREATED="1347705651162" ID="ID_177126448" MODIFIED="1347705661410" TEXT="6. greenhouse">
-<node CREATED="1347705661722" ID="ID_167113002" MODIFIED="1347705671878" TEXT="um lugarzinho onde tem uma certa luz, como uma estufa"/>
-<node CREATED="1347705673431" ID="ID_1537917325" MODIFIED="1347705686927" TEXT="manter o ambiente que favorece o crescimento de ideias"/>
-<node CREATED="1347705687526" ID="ID_829354378" MODIFIED="1347705702149" TEXT="&#xe9; o conceito de come&#xe7;ar com idia mas deixar ela morrer"/>
+<node CREATED="1347806957939" ID="ID_1442284038" MODIFIED="1347806971956" TEXT="um conceito americano que foi dif&#xed;cil de explicar"/>
+<node CREATED="1347705661722" ID="ID_167113002" MODIFIED="1347806973413" TEXT="&#xe9; um lugarzinho onde tem uma certa luz, como uma estufa"/>
+<node CREATED="1347705673431" ID="ID_1537917325" MODIFIED="1347806980331" TEXT="a ideia seria manter o ambiente que favorece o crescimento de ideias"/>
+<node CREATED="1347705687526" ID="ID_829354378" MODIFIED="1347806984703" TEXT="&#xe9; o conceito de come&#xe7;ar com ideia mas deixar ela morrer"/>
 </node>
 <node CREATED="1347705706507" ID="ID_1516430345" MODIFIED="1347705714945" TEXT="7. mantenha-se &#xe0; frente mas n&#xe3;o muito"/>
-<node CREATED="1347705717419" ID="ID_340963023" MODIFIED="1347705730352" TEXT="8. foco no produto m&#xed;nimo vi&#xe1;vel (MVP)">
+<node CREATED="1347705717419" ID="ID_340963023" LINK="http://en.wikipedia.org/wiki/Minimum_viable_product" MODIFIED="1347807022165" TEXT="8. foco no produto m&#xed;nimo vi&#xe1;vel (MVP)">
 <node CREATED="1347705730874" ID="ID_1959625456" MODIFIED="1347705744478" TEXT="&quot;seria legal se tivesse mais aquilo ali&quot; -- N&#xe3;o!"/>
 </node>
 <node CREATED="1347705746776" ID="ID_594463982" MODIFIED="1347705754557" TEXT="9. o software &#xe9; o entreg&#xe1;vel">
@@ -71,7 +75,7 @@
 <node CREATED="1347705840758" ID="ID_1490137344" MODIFIED="1347705844311" TEXT="usu&#xe1;rio usando software!"/>
 </node>
 <node CREATED="1347705848884" ID="ID_1793940233" MODIFIED="1347705853684" POSITION="right" TEXT="como isso &#xe9; diferente?">
-<node CREATED="1347705855284" ID="ID_52677076" MODIFIED="1347705870529" TEXT="todo momento tem que trabalhar para o software funcional como entreg&#xe1;vel"/>
+<node CREATED="1347705855284" ID="ID_52677076" MODIFIED="1347807064118" TEXT="todo momento tem que trabalhar para ter o software funcional como entreg&#xe1;vel"/>
 </node>
 </node>
 </map>
