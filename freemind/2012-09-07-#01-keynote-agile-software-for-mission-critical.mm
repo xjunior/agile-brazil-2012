fe5a2bb24827db3be161dece24243b9db60fcf3e
Daniel Medeiros <medeiros@lavabit.com>
2012-09-16 13:54:19 -0300

    organizacao do mapa mental sobre a palestra do cara da nasa

--- freemind/2012-09-07-#01-keynote-agile-software-for-mission-critical.mm
+++ freemind/2012-09-07-#01-keynote-agile-software-for-mission-critical.mm
@@ -1,72 +1,75 @@
 <map version="0.9.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1347017543084" ID="ID_775715047" MODIFIED="1347017613362" TEXT="Agile software for mission critical operations at the NASA jet propulsion laboratory">
-<node CREATED="1347020147257" ID="ID_1283566946" MODIFIED="1347020154885" POSITION="left" TEXT="Palestante: ">
-<node CREATED="1347022703147" ID="ID_1092409942" MODIFIED="1347022800650" TEXT="khawaja shams"/>
-<node CREATED="1347020193361" ID="ID_979069062" MODIFIED="1347020201927" TEXT="da nasa jet propulsion laboratory"/>
+<node CREATED="1347020147257" ID="ID_1283566946" MODIFIED="1347814102474" POSITION="left" TEXT="palestante">
+<node CREATED="1347022703147" ID="ID_1092409942" MODIFIED="1347813992049" TEXT="Khawaja Shams"/>
+<node CREATED="1347020193361" ID="ID_979069062" MODIFIED="1347814093812" TEXT="da NASA Jet Propulsion Laboratory">
 <node CREATED="1347020203462" ID="ID_942226237" MODIFIED="1347020267962" TEXT="California Institute of Technology"/>
 </node>
-<node CREATED="1347020327786" ID="ID_1348870378" MODIFIED="1347020335455" POSITION="left" TEXT="falou sobre Curiosity">
+</node>
+<node CREATED="1347020327786" ID="ID_1348870378" MODIFIED="1347814117088" POSITION="left" TEXT="iniciou falando sobre &quot;Curiosity&quot;">
 <node CREATED="1347020390794" ID="ID_1832901099" MODIFIED="1347020410234" TEXT="o Rover enviado para Marte"/>
 </node>
-<node CREATED="1347020464411" ID="ID_1412181844" MODIFIED="1347020473851" POSITION="right" TEXT="&#xe9; de miss&#xe3;o cr&#xed;tica"/>
-<node CREATED="1347020536171" ID="ID_366755492" MODIFIED="1347020548294" POSITION="right" TEXT="fotos de gigapiexls tiradas em marte"/>
-<node CREATED="1347020554275" ID="ID_1582055809" MODIFIED="1347020563450" POSITION="right" TEXT="i am here to talk about teams, not software">
-<node CREATED="1347020566170" ID="ID_1704468998" MODIFIED="1347020577061" TEXT="three secret ingredients">
-<node CREATED="1347020588470" ID="ID_768620460" MODIFIED="1347020596537" TEXT="operations planning software laboratory">
+<node CREATED="1347020554275" FOLDED="true" ID="ID_1582055809" MODIFIED="1347814420033" POSITION="right" TEXT="i am here to talk about teams, not software">
+<node CREATED="1347020566170" FOLDED="true" ID="ID_1704468998" MODIFIED="1347814419593" TEXT="three secret ingredients">
+<node CREATED="1347020588470" FOLDED="true" ID="ID_768620460" MODIFIED="1347814418513" TEXT="operations planning software laboratory">
 <node CREATED="1347020597027" ID="ID_91039053" MODIFIED="1347020598433" TEXT="usam agile"/>
 </node>
-<node CREATED="1347020602729" ID="ID_1836142405" MODIFIED="1347020605507" TEXT="environment">
-<node CREATED="1347020605950" ID="ID_947483722" MODIFIED="1347020619150" TEXT="essencial terem um ambiente para colaborar e nao terem distracao"/>
-<node CREATED="1347020713972" ID="ID_792121664" MODIFIED="1347020719060" TEXT="nao usam cubiculo">
-<node CREATED="1347020719503" ID="ID_119501367" MODIFIED="1347020731532" TEXT="dficil pra fazer design em lugar tao pequeno"/>
+<node CREATED="1347020602729" FOLDED="true" ID="ID_1836142405" MODIFIED="1347814291217" TEXT="environment">
+<node CREATED="1347020605950" ID="ID_947483722" MODIFIED="1347814158427" TEXT="essencial terem um ambiente para colaborar e nao terem distra&#xe7;&#xe3;o"/>
+<node CREATED="1347020713972" ID="ID_792121664" MODIFIED="1347814164091" TEXT="nao usam cub&#xed;culo">
+<node CREATED="1347020719503" ID="ID_119501367" MODIFIED="1347814173501" TEXT="dif&#xed;cil fazer design em lugar t&#xe3;o pequeno"/>
 </node>
-<node CREATED="1347020736983" ID="ID_1334357746" MODIFIED="1347020748510" TEXT="onde argmentame  fazem brainstorm?"/>
-<node CREATED="1347020797507" ID="ID_1599746919" MODIFIED="1347020804795" TEXT="ewhat about long bench?">
-<node CREATED="1347020809917" ID="ID_214987664" MODIFIED="1347020817173" TEXT="o cara numa ponta nao tem acesso facil ao cara da outra ponta"/>
-<node CREATED="1347020818240" ID="ID_682242638" MODIFIED="1347020826229" TEXT="com fone de ouvidos, seria o mesmo dos cubiiculos"/>
+<node CREATED="1347020736983" ID="ID_1334357746" MODIFIED="1347814227216" TEXT="onde argumentam e fazem brainstorm?"/>
+<node CREATED="1347020797507" FOLDED="true" ID="ID_1599746919" MODIFIED="1347814287841" TEXT="what about long bench?">
+<node CREATED="1347814191217" ID="ID_1690114848" MODIFIED="1347814209108" TEXT="s&#xe3;o aquelas mesas compridas onde ficam v&#xe1;rias pessoas"/>
+<node CREATED="1347020809917" ID="ID_214987664" MODIFIED="1347814214556" TEXT="o cara numa ponta n&#xe3;o tem acesso f&#xe1;cil ao cara da outra ponta"/>
+<node CREATED="1347020818240" ID="ID_682242638" MODIFIED="1347814218920" TEXT="com fone de ouvidos, seria o mesmo dos cub&#xed;culos"/>
 </node>
-<node CREATED="1347020895366" ID="ID_1392062587" MODIFIED="1347021230629" TEXT="apresentou a sala na nasa: com mesas pr&#xf3;ximas, com lousa e etc">
+<node CREATED="1347020895366" FOLDED="true" ID="ID_1392062587" MODIFIED="1347814289737" TEXT="apresentou a sala da NASA">
+<node CREATED="1347814252332" ID="ID_11219921" MODIFIED="1347814256277" TEXT="com mesas pr&#xf3;ximas"/>
+<node CREATED="1347814256869" ID="ID_1074023029" MODIFIED="1347814268801" TEXT="com lousa para r&#xe1;pidas conversas"/>
 <node CREATED="1347021193343" ID="ID_716282407" MODIFIED="1347021257156" TEXT="em ambiente como este, deve tomar cuidado para n&#xe3;o distrair outras pessoas"/>
 </node>
 <node CREATED="1347020979146" ID="ID_744253149" MODIFIED="1347020994608" TEXT="sugere que as pessoas fa&#xe7;am paradas para caf&#xe9; e almo&#xe7;o para melhor intera&#xe7;&#xe3;o"/>
 </node>
-<node CREATED="1347021289142" ID="ID_895102486" MODIFIED="1347021292139" TEXT="Team structure">
-<node CREATED="1347021294095" ID="ID_802823556" MODIFIED="1347021302146" TEXT="como estruturar o time e identificar lideres?"/>
+<node CREATED="1347021289142" FOLDED="true" ID="ID_895102486" MODIFIED="1347814346094" TEXT="team structure">
+<node CREATED="1347021294095" ID="ID_802823556" MODIFIED="1347814297875" TEXT="como estruturar o time e identificar l&#xed;deres?"/>
 <node CREATED="1347021309125" ID="ID_1790936022" MODIFIED="1347021313667" TEXT="trabalham com muitas miss&#xf5;es"/>
-<node CREATED="1347021325661" ID="ID_1133319238" MODIFIED="1347021330967" TEXT="como suportar essa diversidade de clientes"/>
+<node CREATED="1347021325661" ID="ID_1133319238" MODIFIED="1347814304734" TEXT="como suportar essa diversidade de clientes?"/>
 <node CREATED="1347021378814" ID="ID_757631269" MODIFIED="1347021381609" TEXT="na visao traridional">
 <node CREATED="1347021346327" ID="ID_1871695159" MODIFIED="1347021356719" TEXT="individuos emcarregados de todo um projeto"/>
 </node>
 <node CREATED="1347021386965" ID="ID_654926946" MODIFIED="1347021390930" TEXT="na visao do OPS Lab">
-<node CREATED="1347021411170" ID="ID_1703748190" MODIFIED="1347021431825" TEXT="divferentes individuos trabalham em diferentes prjetos nume diisciplina em especifico"/>
-<node CREATED="1347021464567" ID="ID_1791600907" MODIFIED="1347021477362" TEXT="os times funcionam como incubatoras de startups">
+<node CREATED="1347021411170" ID="ID_1703748190" MODIFIED="1347814343440" TEXT="diferentes indiv&#xed;duos trabalham em diferentes projetos numa diisciplina em espec&#xed;fico"/>
+<node CREATED="1347021464567" ID="ID_1791600907" MODIFIED="1347814329162" TEXT="os times funcionam como incubadoras de startups">
 <node CREATED="1347021493942" ID="ID_1181444527" MODIFIED="1347021499248" TEXT="bom para identificar lideres no time"/>
 <node CREATED="1347021518372" ID="ID_1668862753" MODIFIED="1347021524474" TEXT="permite o publish, present and demo"/>
 </node>
 </node>
 </node>
 <node CREATED="1347021805748" ID="ID_1441324891" MODIFIED="1347021809775" TEXT="strong process">
-<node CREATED="1347021821878" ID="ID_920779655" MODIFIED="1347021826420" TEXT="usam algo bem similar a scrum">
-<node CREATED="1347021862017" ID="ID_1701846918" MODIFIED="1347021867963" TEXT="give transparencu to customers"/>
-<node CREATED="1347021942233" ID="ID_1679155586" MODIFIED="1347021944918" TEXT="work together">
+<node CREATED="1347021821878" FOLDED="true" ID="ID_920779655" MODIFIED="1347814416985" TEXT="usam algo bem similar a scrum">
+<node CREATED="1347021862017" ID="ID_1701846918" MODIFIED="1347814357102" TEXT="give transparency to customers"/>
+<node CREATED="1347021942233" FOLDED="true" ID="ID_1679155586" MODIFIED="1347814374187" TEXT="work together">
 <node CREATED="1347021948262" ID="ID_818892829" MODIFIED="1347021949808" TEXT="todo"/>
-<node CREATED="1347021950797" ID="ID_1501997519" MODIFIED="1347021953311" TEXT="im progres"/>
+<node CREATED="1347021950797" ID="ID_1501997519" MODIFIED="1347814362503" TEXT="in progress"/>
 <node CREATED="1347021954315" ID="ID_498499815" MODIFIED="1347021957179" TEXT="done"/>
-<node CREATED="1347021958433" ID="ID_450441145" MODIFIED="1347021963911" TEXT="tem um servidor para integracao continua"/>
+<node CREATED="1347021958433" ID="ID_450441145" MODIFIED="1347814373129" TEXT="t&#xea;m um servidor para integra&#xe7;&#xe3;o cont&#xed;nua"/>
 </node>
-<node CREATED="1347022015295" ID="ID_1130149747" MODIFIED="1347022027169" TEXT="tentam que o cliente nao coloque um novo item no meio do sprint"/>
+<node CREATED="1347022015295" ID="ID_1130149747" MODIFIED="1347814384987" TEXT="tentam fazer com que o cliente n&#xe3;o coloque um novo item no meio do sprint"/>
 <node CREATED="1347022154307" ID="ID_976765017" MODIFIED="1347022159676" TEXT="fazem daily standups de 15 min"/>
-<node CREATED="1347022368909" ID="ID_1979343617" MODIFIED="1347022384183" TEXT="para motivacao, eles batem palmas quando as tarefas v&#xe3;o de in progress para done"/>
-<node CREATED="1347022449975" ID="ID_441097952" MODIFIED="1347022451599" TEXT="sobre times">
+<node CREATED="1347022368909" ID="ID_1979343617" MODIFIED="1347814396413" TEXT="para motiva&#xe7;&#xe3;o, eles batem palmas quando as tarefas v&#xe3;o de &quot;in progress&quot; para &quot;done&quot;"/>
+<node CREATED="1347022449975" FOLDED="true" ID="ID_441097952" MODIFIED="1347814415568" TEXT="sobre times">
 <node CREATED="1347022421442" ID="ID_60993376" MODIFIED="1347022426249" TEXT="possuem co-located teams"/>
 <node CREATED="1347022455286" ID="ID_1680629431" MODIFIED="1347022459859" TEXT="possuem distribuited teams"/>
 </node>
-<node CREATED="1347022524379" ID="ID_1020592431" MODIFIED="1347022529934" TEXT="fazem retrospectiva de scrums">
+<node CREATED="1347022524379" FOLDED="true" ID="ID_1020592431" MODIFIED="1347814416264" TEXT="fazem retrospectiva de scrums">
 <node CREATED="1347022543598" ID="ID_1958118280" MODIFIED="1347022549489" TEXT="empresa &#xe9; muito grande, tem muitos projetos"/>
 <node CREATED="1347022550384" ID="ID_1334270146" MODIFIED="1347022566930" TEXT="como fazer para garantir que todos est&#xe3;o usando scrum proprieamente?"/>
-<node CREATED="1347022530689" ID="ID_881973911" MODIFIED="1347022541923" TEXT="scum masters fazem reuni&#xf5;es para alinhamento"/>
-<node CREATED="1347022567809" ID="ID_1856541140" MODIFIED="1347022570713" TEXT="scrum of scrums"/>
+<node CREATED="1347022530689" ID="ID_881973911" MODIFIED="1347022541923" TEXT="scum masters fazem reuni&#xf5;es para alinhamento">
+<node CREATED="1347022567809" ID="ID_1856541140" MODIFIED="1347814407208" TEXT="&quot;scrum of scrums&quot;"/>
+</node>
 </node>
 <node CREATED="1347022581475" ID="ID_745908847" MODIFIED="1347022593068" TEXT="&quot;every sprint is a chance to improve&quot;"/>
 <node CREATED="1347022677017" ID="ID_1547357026" MODIFIED="1347022721339" TEXT="fazem prot&#xf3;tipos, como forma de testar tecnologias que podem ser utiizadas"/>
@@ -74,5 +77,7 @@
 </node>
 </node>
 </node>
+<node CREATED="1347020464411" ID="ID_1412181844" MODIFIED="1347020473851" POSITION="left" TEXT="&#xe9; de miss&#xe3;o cr&#xed;tica"/>
+<node CREATED="1347020536171" ID="ID_366755492" MODIFIED="1347814139061" POSITION="left" TEXT="as fotos tiradas em marte s&#xe3;o da ordem de gigapixels"/>
 </node>
 </map>
