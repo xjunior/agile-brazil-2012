1cc35273699d7cd3d7dd6c32f434edeceec39d6a
Daniel Medeiros <medeiros@lavabit.com>
2012-09-16 13:46:11 -0300

    organizacao do mapa mental da palestra de amadurecimento de modelos de gestao de pessoas

--- freemind/2012-09-06-#08-amadurecendo-modelos-de-gestao-de-pessoas.mm
+++ freemind/2012-09-06-#08-amadurecendo-modelos-de-gestao-de-pessoas.mm
@@ -1,7 +1,7 @@
 <map version="0.9.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1346931300118" ID="ID_1296464210" MODIFIED="1346931336606" TEXT="Amadurecendo modelos de gest&#xe3;o de pessoas em uma cultura &#xe1;gil">
-<node CREATED="1347707741217" ID="ID_1120924871" MODIFIED="1347707744333" POSITION="left" TEXT="Palestrantes">
+<node CREATED="1347707741217" FOLDED="true" ID="ID_1120924871" MODIFIED="1347813742819" POSITION="left" TEXT="Palestrantes">
 <node CREATED="1347707745744" ID="ID_1240740711" MODIFIED="1347707754316" TEXT="Raquel Vilas Boas">
 <node CREATED="1347707979599" ID="ID_977225274" MODIFIED="1347707984063" TEXT="quelpsi@gmail.com"/>
 </node>
@@ -9,20 +9,21 @@
 <node CREATED="1347707968144" ID="ID_1911277071" MODIFIED="1347707975653" TEXT="karinne.leissa@gmail.com"/>
 </node>
 <node CREATED="1347707731530" ID="ID_259166219" MODIFIED="1347707764603" TEXT="formadas em psicologia"/>
-<node CREATED="1347707765422" ID="ID_1439399101" MODIFIED="1347707772755" TEXT="da empresa inctacto"/>
+<node CREATED="1347707765422" ID="ID_1439399101" LINK="http://intacto.com.br/" MODIFIED="1347813572510" TEXT="da empresa Intacto"/>
 </node>
-<node CREATED="1347707776934" ID="ID_676246127" MODIFIED="1347707799985" POSITION="left" TEXT="pol&#xed;tica de treinamento">
-<node CREATED="1347707800421" ID="ID_155646175" MODIFIED="1347707800880" TEXT="povo autodidata"/>
+<node CREATED="1347707829735" FOLDED="true" ID="ID_1083509827" MODIFIED="1347813666740" POSITION="left" TEXT="descobriram que mundo de TI gira mais r&#xe1;pido">
+<node CREATED="1347707776934" ID="ID_676246127" MODIFIED="1347813655164" TEXT="pol&#xed;tica de treinamento?">
+<node CREATED="1347707800421" ID="ID_155646175" MODIFIED="1347813657883" TEXT="povo &#xe9; autodidata"/>
 </node>
-<node CREATED="1347707791771" ID="ID_148486552" MODIFIED="1347707797152" POSITION="left" TEXT="sa&#xfa;de do trabalhador">
+<node CREATED="1347707791771" ID="ID_148486552" MODIFIED="1347707797152" TEXT="sa&#xfa;de do trabalhador">
 <node CREATED="1347707804370" ID="ID_501136209" MODIFIED="1347707809271" TEXT="pessoal querendo trabalhar &#xe0; noite"/>
 </node>
-<node CREATED="1347707810529" ID="ID_326235944" MODIFIED="1347707814566" POSITION="left" TEXT="rotatividade">
+<node CREATED="1347707810529" ID="ID_326235944" MODIFIED="1347707814566" TEXT="rotatividade">
 <node CREATED="1347707817138" ID="ID_989659130" MODIFIED="1347707824541" TEXT="mundo de TI &#xe9; r&#xe1;pido em rotatividade"/>
 </node>
-<node CREATED="1347707829735" ID="ID_1083509827" MODIFIED="1347707839436" POSITION="left" TEXT="descobriram que mundo de TI gira mais r&#xe1;pido"/>
-<node CREATED="1347707843830" ID="ID_1947592003" MODIFIED="1347707849411" POSITION="left" TEXT="modelos em gest&#xe3;o de pessoas">
-<node CREATED="1347707852029" ID="ID_21598640" MODIFIED="1347707858122" TEXT="s&#xe3;o concep&#xe7;&#xf5;es">
+</node>
+<node CREATED="1347707843830" FOLDED="true" ID="ID_1947592003" MODIFIED="1347813910914" POSITION="left" TEXT="modelos em gest&#xe3;o de pessoas">
+<node CREATED="1347707852029" ID="ID_21598640" MODIFIED="1347813908058" TEXT="s&#xe3;o concep&#xe7;&#xf5;es">
 <node CREATED="1347707860700" ID="ID_785743275" MODIFIED="1347707866393" TEXT="formas de entender as pessoas, o mundo"/>
 <node CREATED="1347707903239" ID="ID_528822375" MODIFIED="1347707915956" TEXT="modelos baseiam a pr&#xe1;tica, e convivem">
 <node CREATED="1347707871171" ID="ID_1210681555" MODIFIED="1347707874536" TEXT="cuidadores do sistema"/>
@@ -31,12 +32,12 @@
 </node>
 </node>
 </node>
-<node CREATED="1347707994870" ID="ID_842723132" MODIFIED="1347707997803" POSITION="right" TEXT="gestores de rh">
+<node CREATED="1347707994870" FOLDED="true" ID="ID_842723132" MODIFIED="1347813720286" POSITION="right" TEXT="gestores de rh">
 <node CREATED="1347707999541" ID="ID_1477590071" MODIFIED="1347708005282" TEXT="vis&#xe3;o de caixas dentro de caixas">
 <node CREATED="1347708011036" ID="ID_701163225" MODIFIED="1347708022633" TEXT="separa&#xe7;&#xe3;o r&#xed;gida entre vida dentro e fora do trabalho"/>
 </node>
 </node>
-<node CREATED="1347708024050" ID="ID_1208787676" MODIFIED="1347708030656" POSITION="right" TEXT="cuidadores do sistema">
+<node CREATED="1347708024050" FOLDED="true" ID="ID_1208787676" MODIFIED="1347813734865" POSITION="right" TEXT="cuidadores do sistema">
 <node CREATED="1347708031977" ID="ID_1552906437" MODIFIED="1347708042927" TEXT="n&#xe3;o aceita rela&#xe7;&#xe3;o de causa e efeito"/>
 <node CREATED="1347708046744" ID="ID_1013455561" MODIFIED="1347708050262" TEXT="vis&#xe3;o de mundo mais complexa"/>
 <node CREATED="1347708052719" ID="ID_230253887" MODIFIED="1347708062084" TEXT="rela&#xe7;&#xe3;o entre vida e trabalho &#xe9; mais fina">
@@ -49,7 +50,7 @@
 <node CREATED="1347708102378" ID="ID_720910479" MODIFIED="1347708114619" TEXT="cuidar sistema n&#xe3;o atende bem demandas r&#xe1;pidas"/>
 <node CREATED="1347708117512" ID="ID_659127067" MODIFIED="1347708125206" TEXT="funciona bem em cen&#xe1;rios mais complexos"/>
 </node>
-<node CREATED="1347708126047" ID="ID_1235397720" MODIFIED="1347708151323" POSITION="right" TEXT="agentes provocadores de melhoria cont&#xed;nua">
+<node CREATED="1347708126047" FOLDED="true" ID="ID_1235397720" MODIFIED="1347813759062" POSITION="right" TEXT="agentes provocadores de melhoria cont&#xed;nua">
 <node CREATED="1347708161891" ID="ID_7370425" MODIFIED="1347708173593" TEXT="preocupa&#xe7;&#xe3;o &#xe9; que vida e trabalho est&#xe3;o juntos">
 <node CREATED="1347708174146" ID="ID_929918850" MODIFIED="1347708183128" TEXT="vou passar grande parte da vida no trabalho, ent&#xe3;o quero gostar do trabalho"/>
 </node>
@@ -58,11 +59,11 @@
 <node CREATED="1347708208191" ID="ID_622148944" MODIFIED="1347708220716" TEXT="rh n&#xe3;o faz sentido, e sim pessoas capazes de provocar melhoria"/>
 <node CREATED="1347708223517" ID="ID_1836947015" MODIFIED="1347708237722" TEXT="esse modelo n&#xe3;o funciona para atender uma necessidade imediata da organiza&#xe7;&#xe3;o"/>
 </node>
-<node CREATED="1347707934420" ID="ID_1398359230" MODIFIED="1347707938801" POSITION="left" TEXT="pessoa - organiza&#xe7;&#xe3;o - pessoa">
+<node CREATED="1347707934420" FOLDED="true" ID="ID_1398359230" MODIFIED="1347813741987" POSITION="left" TEXT="pessoa - organiza&#xe7;&#xe3;o - pessoa">
 <node CREATED="1347707939515" ID="ID_775850726" MODIFIED="1347707946632" TEXT="pessoas se desenvolvem em espiral"/>
 <node CREATED="1347707952690" ID="ID_1913867091" MODIFIED="1347707959577" TEXT="minha a&#xe7;&#xe3;o no mundo transforma o mundo e me transforma"/>
 </node>
-<node CREATED="1347708240499" ID="ID_1329250317" MODIFIED="1347708242915" POSITION="right" TEXT="princ&#xed;pios">
+<node CREATED="1347708240499" FOLDED="true" ID="ID_1329250317" MODIFIED="1347813774729" POSITION="right" TEXT="princ&#xed;pios">
 <node CREATED="1347708244467" ID="ID_581023900" MODIFIED="1347708246625" TEXT="protagonismo">
 <node CREATED="1347708248314" ID="ID_1601008061" MODIFIED="1347708258192" TEXT="pessoas s&#xe3;o capazes de transformar a realidade."/>
 <node CREATED="1347708259737" ID="ID_1125565807" MODIFIED="1347708263824" TEXT="esp&#xe9;cie de empreendedorismo"/>
@@ -81,7 +82,7 @@
 </node>
 </node>
 </node>
-<node CREATED="1347708412393" ID="ID_1988875111" MODIFIED="1347708416224" POSITION="right" TEXT="princ&#xed;pios na pr&#xe1;tica">
+<node CREATED="1347708412393" FOLDED="true" ID="ID_1988875111" MODIFIED="1347813783636" POSITION="right" TEXT="princ&#xed;pios na pr&#xe1;tica">
 <node CREATED="1347708417952" ID="ID_1661091488" MODIFIED="1347708423543" TEXT="dar liberdade com responsabilidade">
 <node CREATED="1347708424847" ID="ID_236986795" MODIFIED="1347708431480" TEXT="protagonista encontrar seu espa&#xe7;o"/>
 <node CREATED="1347708438574" ID="ID_35559125" MODIFIED="1347708441615" TEXT="mind fullness">
@@ -95,7 +96,7 @@
 <node CREATED="1347708476154" ID="ID_1974327255" MODIFIED="1347708481375" TEXT="precisa de flexibilidade neste modelo"/>
 </node>
 </node>
-<node CREATED="1347708485385" ID="ID_1987979247" MODIFIED="1347708492312" POSITION="right" TEXT="ser&#xe1; que estou pronto para este modelo de provocadores?">
+<node CREATED="1347708485385" FOLDED="true" ID="ID_1987979247" MODIFIED="1347813833780" POSITION="right" TEXT="ser&#xe1; que estou pronto para este modelo de provocadores?">
 <node CREATED="1347708492824" ID="ID_504911325" MODIFIED="1347708503769" TEXT="forne&#xe7;am novas pessoas &#xe0; equipe"/>
 <node CREATED="1347708505663" ID="ID_1847449721" MODIFIED="1347708513382" TEXT="n&#xe3;o aprisionar as pessoas">
 <node CREATED="1347708513901" ID="ID_1019335842" MODIFIED="1347708517438" TEXT="deixar ela achar um sentido"/>
@@ -104,14 +105,14 @@
 <node CREATED="1347708682083" ID="ID_1021359254" MODIFIED="1347708686972" TEXT="as lutas tem que ser parte de um movimento"/>
 </node>
 </node>
-<node CREATED="1347708692410" ID="ID_974182782" MODIFIED="1347708699277" POSITION="right" TEXT="livro: management 3.0">
-<node CREATED="1347708701050" ID="ID_1232870179" MODIFIED="1347708704796" TEXT="jurgen appelo"/>
-<node CREATED="1347708708177" ID="ID_1951999328" MODIFIED="1347708713098" TEXT="essa palestra segue o livro"/>
+<node CREATED="1347708692410" FOLDED="true" ID="ID_974182782" LINK="http://www.amazon.com/Management-3-0-Developers-Developing-Addison-Wesley/dp/0321712471/ref=sr_1_1?ie=UTF8&amp;qid=1347813844&amp;sr=8-1&amp;keywords=management+3.0" MODIFIED="1347813898384" POSITION="right" TEXT="livro citado: &quot;Management 3.0: Leading Agile Developers, Developing Agile Leaders&quot;">
+<node CREATED="1347708701050" ID="ID_1232870179" MODIFIED="1347813867578" TEXT="de jurgen appelo"/>
+<node CREATED="1347708708177" ID="ID_1951999328" MODIFIED="1347813877244" TEXT="essa palestra, de certa forma, segue o livro"/>
 </node>
-<node CREATED="1347708716560" ID="ID_1728332356" MODIFIED="1347708727111" POSITION="right" TEXT="elas n&#xe3;o viram defender o 2o. e 3o. modelos">
+<node CREATED="1347708716560" FOLDED="true" ID="ID_1728332356" MODIFIED="1347813897584" POSITION="right" TEXT="elas n&#xe3;o viram defender o 2o. e 3o. modelos">
 <node CREATED="1347708727615" ID="ID_1264154155" MODIFIED="1347708736134" TEXT="mesmo parecendo ser melhor para &#xe1;gil"/>
 <node CREATED="1347708737318" ID="ID_851651900" MODIFIED="1347708757228" TEXT="princ&#xed;pio que fica &#xe9; o do Kaisen: da melhoria cont&#xed;nua"/>
-<node CREATED="1347708759619" ID="ID_496380144" MODIFIED="1347708779499" TEXT="transitar entre estes modelos de forma que construa um contexto que fa&#xe7;a sentido"/>
+<node CREATED="1347708759619" ID="ID_496380144" MODIFIED="1347813895095" TEXT="importante transitar entre estes modelos de forma que construa um contexto que fa&#xe7;a sentido"/>
 </node>
 </node>
 </map>
