6732e0f04f8247bce19842d0ed9f44751aaa20bf
Daniel Medeiros <medeiros@lavabit.com>
2012-09-16 13:17:25 -0300

    organizacao do mapa mental da palestra 'the changing role of a software developer'

--- freemind/2012-09-06-#05-the-changing-role-of-a-software-developer.mm
+++ freemind/2012-09-06-#05-the-changing-role-of-a-software-developer.mm
@@ -1,19 +1,34 @@
 <map version="0.9.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1346931109233" ID="ID_177803409" MODIFIED="1346955764869" TEXT="The changing role of a software developer: How our approach to building the right product continues to evolve">
-<node CREATED="1346953081333" ID="ID_50833666" MODIFIED="1346953092083" POSITION="right" TEXT="how do we build the right thing?">
+<node CREATED="1346954015948" ID="ID_413237614" MODIFIED="1346954037993" POSITION="right" TEXT="levantou a quest&#xe3;o de pessoas que concordam num ponto, mas entendem diferentemente"/>
+<node CREATED="1346954219739" ID="ID_323891758" LINK="http://www.infoq.com/presentations/Co-Making-Great-Products" MODIFIED="1347812148501" POSITION="right" TEXT="surge a id&#xe9;ia de co-making software, com Jeff Patton"/>
+<node CREATED="1346954792104" FOLDED="true" ID="ID_1474194662" MODIFIED="1347812195738" POSITION="right" TEXT="est&#xf3;ria de um trabalho em equipe">
+<node CREATED="1346954803734" ID="ID_1516091425" MODIFIED="1346954811661" TEXT="desenvolvedor e pessoa de neg&#xf3;cio"/>
+<node CREATED="1346954812540" ID="ID_980422419" MODIFIED="1346954822667" TEXT="criando um game com muitos downloads"/>
+<node CREATED="1346954826643" ID="ID_866515400" MODIFIED="1347812163853" TEXT="mantemdo foco no importante"/>
+</node>
+<node CREATED="1346955589788" FOLDED="true" ID="ID_1748247415" MODIFIED="1347812196346" POSITION="right" TEXT="I just want to code">
+<node CREATED="1346955599078" ID="ID_1179374476" MODIFIED="1346955608362" TEXT="developers outside their confort zone"/>
+<node CREATED="1347812173256" ID="ID_1405776890" MODIFIED="1347812186513" TEXT="no fim, todos queremos programar, mas outras responsabilidades chamam"/>
+</node>
+<node CREATED="1346955642267" ID="ID_648622053" MODIFIED="1346955782192" POSITION="left" TEXT="Palestrante: Dr Angela Martin">
+<node CREATED="1346955648827" ID="ID_720265743" MODIFIED="1346955652775" TEXT="angela.m.martin@gmail.com"/>
+</node>
+<node CREATED="1347812024876" ID="ID_1096974383" MODIFIED="1347812040302" POSITION="left" TEXT="esta apresenta&#xe7;&#xe3;o foi mais um relato pessoal de uma profissional da &#xe1;rea"/>
+<node CREATED="1346953081333" FOLDED="true" ID="ID_50833666" MODIFIED="1347812192473" POSITION="left" TEXT="how do we build the right thing?">
 <node CREATED="1346953304803" ID="ID_1696684337" MODIFIED="1346953315265" TEXT="gostava muito de programar qdo adolescente"/>
 <node CREATED="1346953325910" ID="ID_1186446731" MODIFIED="1346953351748" TEXT="graduou-se, entrou para uma empresa e desenvolveu um sistema"/>
-<node CREATED="1346953352596" ID="ID_895464963" MODIFIED="1346953368151" TEXT="depois de 2 anos, pediram para criar um sistema que era igual &#xe0;quele">
+<node CREATED="1346953352596" ID="ID_895464963" MODIFIED="1347812051857" TEXT="depois de 2 anos, pediram para criar um sistema que era igual ao que havia criado">
 <node CREATED="1346953369686" ID="ID_1335526839" MODIFIED="1346953377800" TEXT="ou seja, ninguem sabia que o primeiro existia!">
 <node CREATED="1346953378320" ID="ID_182258779" MODIFIED="1346953380257" TEXT="nao era usado"/>
 </node>
 </node>
 <node CREATED="1346953382712" ID="ID_86565416" MODIFIED="1346953394523" TEXT="decep&#xe7;&#xe3;o, e a sensa&#xe7;&#xe3;o de que n&#xe3;o estava construindo a coisa certa"/>
-<node CREATED="1346953436485" ID="ID_889745458" MODIFIED="1346953443944" TEXT="come&#xe7;ou a entender os usuarios finais"/>
 </node>
-<node CREATED="1346953489354" ID="ID_203427100" MODIFIED="1346953498926" POSITION="right" TEXT="there has to be a better way">
-<node CREATED="1346953711771" ID="ID_55970649" MODIFIED="1346953737833" TEXT="come&#xe7;aram a trabalharcome&#xe7;ou a trabalhar mais proximo do cliente"/>
+<node CREATED="1346953489354" FOLDED="true" ID="ID_203427100" MODIFIED="1347812193042" POSITION="left" TEXT="there has to be a better way">
+<node CREATED="1346953436485" ID="ID_889745458" MODIFIED="1347812079947" TEXT="come&#xe7;ou a entender os usu&#xe1;rios finais"/>
+<node CREATED="1346953711771" ID="ID_55970649" MODIFIED="1347812073164" TEXT="come&#xe7;ou a trabalhar mais proximo do cliente"/>
 <node CREATED="1346953723713" ID="ID_1779084074" MODIFIED="1346953733683" TEXT="mas ainda n&#xe3;o havia um trabalho real de parceria">
 <node CREATED="1346953738822" ID="ID_1396805724" MODIFIED="1346953744720" TEXT="analogia do driver">
 <node CREATED="1346953745927" ID="ID_1735147083" MODIFIED="1346953748410" TEXT="o cliente eh o driver"/>
@@ -21,28 +36,6 @@
 <node CREATED="1346953754679" ID="ID_168150364" MODIFIED="1346953772496" TEXT="quando o carro capota, o PO nao assume a culpa, j&#xe1; que a dire&#xe7;&#xe3;o era do cliente"/>
 </node>
 </node>
-</node>
-<node CREATED="1346954015948" ID="ID_413237614" MODIFIED="1346954037993" POSITION="right" TEXT="levantou a quest&#xe3;o de pessoas que concordam num ponto, mas entendem diferentemente"/>
-<node CREATED="1346954219739" ID="ID_323891758" MODIFIED="1346954230209" POSITION="right" TEXT="surge a id&#xe9;ia de co-making software">
-<node CREATED="1346954232656" ID="ID_429710593" MODIFIED="1346954235326" TEXT="com jeff patton">
-<node CREATED="1346954240893" ID="ID_32138179" MODIFIED="1346954245247" TEXT="pesquisar">
-<icon BUILTIN="messagebox_warning"/>
-</node>
-</node>
-</node>
-<node CREATED="1346954792104" ID="ID_1474194662" MODIFIED="1346954803120" POSITION="right" TEXT="est&#xf3;ria de um trabalho em equipe">
-<node CREATED="1346954803734" ID="ID_1516091425" MODIFIED="1346954811661" TEXT="desenvolvedor e pessoa de neg&#xf3;cio"/>
-<node CREATED="1346954812540" ID="ID_980422419" MODIFIED="1346954822667" TEXT="criando um game com muitos downloads"/>
-<node CREATED="1346954826643" ID="ID_866515400" MODIFIED="1346954829297" TEXT="foco no importante"/>
-</node>
-<node CREATED="1346955589788" ID="ID_1748247415" MODIFIED="1346955625039" POSITION="right" TEXT="I just want to code">
-<node CREATED="1346955599078" ID="ID_1179374476" MODIFIED="1346955608362" TEXT="developers outside their confort zone"/>
-</node>
-<node CREATED="1346955642267" ID="ID_648622053" MODIFIED="1346955782192" POSITION="left" TEXT="Palestrante: Dr Angela Martin">
-<node CREATED="1346955648827" ID="ID_720265743" MODIFIED="1346955652775" TEXT="angela.m.martin@gmail.com"/>
-</node>
-<node CREATED="1346955696765" ID="ID_597729656" MODIFIED="1346955702883" POSITION="right" TEXT="foi bsaicamente uma opini&#xe3;o pessoal">
-<node CREATED="1346955703466" ID="ID_1272539560" MODIFIED="1346955707773" TEXT="n&#xe3;o foi uma apresenta&#xe7;&#xe3;o muito boa"/>
 </node>
 </node>
 </map>
