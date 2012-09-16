f3fe9d5a098b99779467a4c551a505152c7db955
Daniel Medeiros <medeiros@lavabit.com>
2012-09-16 11:59:38 -0300

    organizacao do mapa mental da apresentacao de desafios na gestao de equipes virtuais

--- freemind/2012-09-05-#07-desafio-gestao-equipes-virtuais.mm
+++ freemind/2012-09-05-#07-desafio-gestao-equipes-virtuais.mm
@@ -1,27 +1,25 @@
 <map version="0.9.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1347705877306" ID="ID_485104163" MODIFIED="1347705915938" TEXT="Desafios na gest&#xe3;o de equipes virtuais">
-<node CREATED="1347705916462" ID="ID_1027915258" MODIFIED="1347705925612" POSITION="left" TEXT="Palestrante: Andreano Lanusse">
-<node CREATED="1347705951393" ID="ID_171533395" MODIFIED="1347705958046" TEXT="da Rally Software">
+<node CREATED="1347705916462" FOLDED="true" ID="ID_1027915258" MODIFIED="1347807375575" POSITION="left" TEXT="Palestrante: Andreano Lanusse">
+<node CREATED="1347705951393" ID="ID_171533395" LINK="http://www.rallydev.com/" MODIFIED="1347807280205" TEXT="da Rally Software">
 <node CREATED="1347706579230" ID="ID_638199949" MODIFIED="1347706583587" TEXT="rallydev.com">
 <node CREATED="1347706590189" ID="ID_356340255" MODIFIED="1347706596396" TEXT="/help/learn_agile"/>
 </node>
 <node CREATED="1347706600452" ID="ID_469312930" MODIFIED="1347706603052" TEXT="/agileblog"/>
 <node CREATED="1347706606915" ID="ID_1940572158" MODIFIED="1347706609610" TEXT="@rallysoftware"/>
-</node>
-<node CREATED="1347706561160" ID="ID_1933918043" MODIFIED="1347706575583" TEXT="andreanolanusse.com">
-<node CREATED="1347706613010" ID="ID_1850655216" MODIFIED="1347706617490" TEXT="@andreanolanusse"/>
-</node>
-</node>
-<node CREATED="1347705945314" ID="ID_694722081" MODIFIED="1347705967637" POSITION="left" TEXT="experi&#xea;ncia com equipes virtuais e como superar desafios">
-<node CREATED="1347705977335" ID="ID_1348514972" MODIFIED="1347705982140" TEXT="Plataforma Agile ALM unificada">
+<node CREATED="1347705945314" ID="ID_694722081" MODIFIED="1347705967637" TEXT="experi&#xea;ncia com equipes virtuais e como superar desafios">
+<node CREATED="1347705977335" ID="ID_1348514972" MODIFIED="1347705982140" TEXT="Plataforma Agile ALM unificada"/>
 <node CREATED="1347705986190" ID="ID_1232104070" MODIFIED="1347705989522" TEXT="alinhamento com servi&#xe7;os"/>
 </node>
 </node>
-<node CREATED="1347705995780" ID="ID_117538143" MODIFIED="1347706006777" POSITION="left" TEXT="produtos s&#xe3;o desenvolvidos de forma distribu&#xed;da">
-<node CREATED="1347706007043" ID="ID_563798561" MODIFIED="1347706010608" TEXT="em locais f&#xed;sicos diferentes"/>
+<node CREATED="1347706561160" ID="ID_1933918043" LINK="http://www.andreanolanusse.com" MODIFIED="1347807319815" TEXT="andreanolanusse.com">
+<node CREATED="1347706613010" ID="ID_1850655216" MODIFIED="1347706617490" TEXT="@andreanolanusse"/>
 </node>
-<node CREATED="1347706019522" ID="ID_935782378" MODIFIED="1347706021015" POSITION="left" TEXT="motivos">
+</node>
+<node CREATED="1347705995780" FOLDED="true" ID="ID_117538143" MODIFIED="1347807408037" POSITION="left" TEXT="produtos s&#xe3;o desenvolvidos de forma distribu&#xed;da">
+<node CREATED="1347706007043" ID="ID_563798561" MODIFIED="1347706010608" TEXT="em locais f&#xed;sicos diferentes"/>
+<node CREATED="1347706019522" ID="ID_935782378" MODIFIED="1347706021015" TEXT="motivos">
 <node CREATED="1347706021900" ID="ID_1853302723" MODIFIED="1347706038623" TEXT="aumenta produtividade"/>
 <node CREATED="1347706030697" ID="ID_1015539137" MODIFIED="1347706037229" TEXT="oportunidade em mercados emergentes"/>
 <node CREATED="1347706039344" ID="ID_570672118" MODIFIED="1347706043047" TEXT="redu&#xe7;&#xe3;o de custos"/>
@@ -30,34 +28,17 @@
 <node CREATED="1347706061965" ID="ID_1998072642" MODIFIED="1347706066043" TEXT="entregar coisas mais r&#xe1;pido"/>
 </node>
 </node>
-<node CREATED="1347706073460" ID="ID_1159334190" MODIFIED="1347706077025" POSITION="left" TEXT="problema">
+<node CREATED="1347706073460" ID="ID_1159334190" MODIFIED="1347706077025" TEXT="problema">
 <node CREATED="1347706077772" ID="ID_1531142910" MODIFIED="1347706084873" TEXT="o que fazer ao querer uma revis&#xe3;o de custo?"/>
 </node>
-<node CREATED="1347706232771" ID="ID_100280389" MODIFIED="1347706239777" POSITION="right" TEXT="co-location = F&#xe1;cil"/>
-<node CREATED="1347706240394" ID="ID_527785421" MODIFIED="1347706275909" POSITION="right" TEXT="dificuldade &#xe9; em part-time co-located e da&#xed; para cima no aumento de distribui&#xe7;&#xe3;o"/>
-<node CREATED="1347706281566" ID="ID_247853115" MODIFIED="1347706285580" POSITION="right" TEXT="necess&#xe1;rio comunica&#xe7;&#xe3;o efetiva">
-<node CREATED="1347706288157" ID="ID_885489340" MODIFIED="1347706298868" TEXT="n&#xe3;o &#xe9; s&#xf3; ligar (fone)"/>
-<node CREATED="1347706300980" ID="ID_493013049" MODIFIED="1347706311849" TEXT="&#xe9; trabalhar de forma conjunta (todos os envolvidos)"/>
-<node CREATED="1347706312458" ID="ID_1752188757" MODIFIED="1347706319568" TEXT="s&#xe3;o mais do que palavras"/>
 </node>
-<node CREATED="1347706323634" ID="ID_1855181478" MODIFIED="1347706327400" POSITION="right" TEXT="confian&#xe7;a"/>
-<node CREATED="1347706328473" ID="ID_54311844" MODIFIED="1347706335943" POSITION="right" TEXT="equipe (senso de equipe)">
-<node CREATED="1347706346794" ID="ID_984737152" MODIFIED="1347706356340" TEXT="defini&#xe7;&#xe3;o de princ&#xed;pios">
-<node CREATED="1347706356838" ID="ID_328987158" MODIFIED="1347706359740" TEXT="compartilhar conhecimentos"/>
-<node CREATED="1347706362333" ID="ID_1009122057" MODIFIED="1347706370555" TEXT="premia&#xe7;&#xe3;o de pessoas">
-<node CREATED="1347706381755" ID="ID_358678248" MODIFIED="1347706404280" TEXT="&quot;apreciation&quot;">
-<node CREATED="1347706384012" ID="ID_1623184357" MODIFIED="1347706405511" TEXT="em reuni&#xf5;es semanais"/>
-</node>
-</node>
-</node>
-</node>
-<node CREATED="1347706095042" ID="ID_1978626966" MODIFIED="1347706099887" POSITION="left" TEXT="desafios em equipes virtuais">
+<node CREATED="1347706095042" FOLDED="true" ID="ID_1978626966" MODIFIED="1347807538605" POSITION="left" TEXT="desafios em equipes virtuais">
 <node CREATED="1347706100745" ID="ID_545322446" MODIFIED="1347706103639" TEXT="timezone"/>
 <node CREATED="1347706104217" ID="ID_1914120797" MODIFIED="1347706107678" TEXT="idioma"/>
 <node CREATED="1347706108240" ID="ID_752432808" MODIFIED="1347706116045" TEXT="feriados"/>
 <node CREATED="1347706117040" ID="ID_1681086177" MODIFIED="1347706118605" TEXT="leis locais"/>
 </node>
-<node CREATED="1347706130726" ID="ID_264024374" MODIFIED="1347706135500" POSITION="left" TEXT="desafios em comunica&#xe7;&#xe3;o virtual">
+<node CREATED="1347706130726" FOLDED="true" ID="ID_264024374" MODIFIED="1347807445940" POSITION="left" TEXT="desafios em comunica&#xe7;&#xe3;o virtual">
 <node CREATED="1347706120743" ID="ID_336635078" MODIFIED="1347706126109" TEXT="tecnologia n&#xe3;o &#xe9; um problema!"/>
 <node CREATED="1347706140765" ID="ID_167524081" MODIFIED="1347706144347" TEXT="linguagem corporal">
 <node CREATED="1347706145460" ID="ID_1160521219" MODIFIED="1347706150402" TEXT="&#xe9; 70% da comunica&#xe7;&#xe3;o"/>
@@ -67,24 +48,44 @@
 <node CREATED="1347706219029" ID="ID_1666390057" MODIFIED="1347706220658" TEXT="impessoal"/>
 <node CREATED="1347706225804" ID="ID_587405742" MODIFIED="1347706229042" TEXT="cria senso de isolamento"/>
 </node>
+<node CREATED="1347706232771" ID="ID_100280389" MODIFIED="1347706239777" TEXT="co-location = F&#xe1;cil">
+<node CREATED="1347706240394" ID="ID_527785421" MODIFIED="1347706275909" TEXT="dificuldade &#xe9; em part-time co-located e da&#xed; para cima no aumento de distribui&#xe7;&#xe3;o"/>
 </node>
-<node CREATED="1347706412928" ID="ID_69796254" MODIFIED="1347706416998" POSITION="right" TEXT="compartilhar responsabilidades"/>
-<node CREATED="1347706420241" ID="ID_572444568" MODIFIED="1347706421957" POSITION="right" TEXT="relacionamento">
-<node CREATED="1347706423943" ID="ID_343879573" MODIFIED="1347706437165" TEXT="n&#xe3;o &#xe9; pr&#xf3;ximo do ambiente de trabalho fora do Brasil"/>
+<node CREATED="1347706281566" ID="ID_247853115" MODIFIED="1347706285580" TEXT="necess&#xe1;rio comunica&#xe7;&#xe3;o efetiva">
+<node CREATED="1347706288157" ID="ID_885489340" MODIFIED="1347706298868" TEXT="n&#xe3;o &#xe9; s&#xf3; ligar (fone)"/>
+<node CREATED="1347706300980" ID="ID_493013049" MODIFIED="1347706311849" TEXT="&#xe9; trabalhar de forma conjunta (todos os envolvidos)"/>
+<node CREATED="1347706312458" ID="ID_1752188757" MODIFIED="1347706319568" TEXT="s&#xe3;o mais do que palavras"/>
+</node>
+</node>
+<node CREATED="1347807471051" FOLDED="true" ID="ID_424235014" MODIFIED="1347807537048" POSITION="right" TEXT="aspectos de equipes virtuais">
+<node CREATED="1347706328473" ID="ID_54311844" MODIFIED="1347807465799" TEXT="equipe (senso de equipe)">
+<node CREATED="1347706346794" ID="ID_984737152" MODIFIED="1347706356340" TEXT="defini&#xe7;&#xe3;o de princ&#xed;pios">
+<node CREATED="1347706356838" ID="ID_328987158" MODIFIED="1347706359740" TEXT="compartilhar conhecimentos"/>
+<node CREATED="1347706362333" ID="ID_1009122057" MODIFIED="1347706370555" TEXT="premia&#xe7;&#xe3;o de pessoas">
+<node CREATED="1347706381755" ID="ID_358678248" MODIFIED="1347706404280" TEXT="&quot;apreciation&quot;">
+<node CREATED="1347706384012" ID="ID_1623184357" MODIFIED="1347706405511" TEXT="em reuni&#xf5;es semanais"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1347706412928" ID="ID_69796254" MODIFIED="1347706416998" TEXT="compartilhar responsabilidades"/>
+<node CREATED="1347706420241" ID="ID_572444568" MODIFIED="1347706421957" TEXT="relacionamento">
+<node CREATED="1347706423943" ID="ID_343879573" MODIFIED="1347807523150" TEXT="fora do Brasil, o relacionamento pessoal e profissional coisas bem distintas"/>
 <node CREATED="1347706440133" ID="ID_1732260866" MODIFIED="1347706450939" TEXT="gerar confian&#xe7;a &#xe9; importante em equipes virtuais"/>
 <node CREATED="1347706451563" ID="ID_1493598845" MODIFIED="1347706453818" TEXT="happy hours"/>
 </node>
-<node CREATED="1347706457067" ID="ID_465515179" MODIFIED="1347706458818" POSITION="right" TEXT="criatividade">
+<node CREATED="1347706457067" ID="ID_465515179" MODIFIED="1347706458818" TEXT="criatividade">
 <node CREATED="1347706462354" ID="ID_1809363978" MODIFIED="1347706467817" TEXT="n&#xe3;o cortar as id&#xe9;ias das pessoas"/>
 <node CREATED="1347706471017" ID="ID_1164977825" MODIFIED="1347706483354" TEXT="encontre formas colaborativas para discuss&#xe3;o em conjunto"/>
 </node>
-<node CREATED="1347706486848" ID="ID_1297055826" MODIFIED="1347706488743" POSITION="right" TEXT="tecnologia">
+<node CREATED="1347706486848" ID="ID_1297055826" MODIFIED="1347706488743" TEXT="tecnologia">
 <node CREATED="1347706490687" ID="ID_863545782" MODIFIED="1347706492578" TEXT="use e abuse"/>
 <node CREATED="1347706497655" ID="ID_1157795045" MODIFIED="1347706501589" TEXT="exemplo: Skype"/>
 <node CREATED="1347706502126" ID="ID_1506264787" MODIFIED="1347706514917" TEXT="muitas oportunidades de colabora&#xe7;&#xe3;o"/>
 </node>
-<node CREATED="1347706519012" ID="ID_968535728" MODIFIED="1347706520495" POSITION="right" TEXT="delegue">
+<node CREATED="1347706519012" ID="ID_968535728" MODIFIED="1347706520495" TEXT="delegue">
 <node CREATED="1347706522996" ID="ID_1691453681" MODIFIED="1347706528348" TEXT="maior oportunidade de criar l&#xed;deres"/>
+</node>
 </node>
 <node CREATED="1347706540162" ID="ID_1292542363" MODIFIED="1347706550632" POSITION="right" TEXT="mudaram de srum para kanban por motivos deles">
 <node CREATED="1347706553769" ID="ID_1361337176" MODIFIED="1347706557065" TEXT="n&#xe3;o quer dizer que seja melhor"/>
